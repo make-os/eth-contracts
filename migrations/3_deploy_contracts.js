8b46368872f3da9ea18cd0fe15e0ecb6ea360de1
Kennedy <kennedyidialu@gmail.com>
2021-02-21 20:16:49 +0100

    phase 1 deployment

--- migrations/3_deploy_contracts.js
+++ migrations/3_deploy_contracts.js
@@ -6,13 +6,14 @@ module.exports = async function (deployer, network, accounts) {
 	const sender = accounts[0]; // change this;
 
 	// Deploy Main contract
-	const ELLContractAddr = "0x61872c4a40D70586A6469dF04d01d00B66580777";
-	const MaxSwappableELL = web3.utils.toWei("18984565"); // 18984565
+	const ELLContractAddr = "0xd138d4813f23bf3bafc6a8e35e2515d8681bd3f6";
+	const MaxSwappableELL = web3.utils.toWei("19000000");
 	await deployer.deploy(Main, MaxSwappableELL, ELLContractAddr, Dilithium.address, {
 		from: sender,
 	});
 
 	// Set the Main contract as the new owner of the Dilithium contract to Maim
 	const dil = await Dilithium.deployed();
-	await dil.setOwner(Main.address, { from: sender });
+	const tx = await dil.setOwner(Main.address, { from: sender });
+	console.log("Set DIL Contract Owner: ", tx.hash);
 };
--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,7 @@
-const HDWalletProvider = require("@truffle/hdwallet-provider");
+const HDWallet = require("@truffle/hdwallet-provider");
+const web3 = require("web3");
+const mnemonic = process.env.MNEMONIC;
+const providerAddr = process.env.PROVIDER_ADDR;
 
 module.exports = {
 	plugins: ["solidity-coverage"],
@@ -14,17 +17,22 @@ module.exports = {
 		},
 	},
 	networks: {
+		live: {
+			provider: () => {
+				return new HDWallet(mnemonic, providerAddr);
+			},
+			gasPrice: web3.utils.toWei("120", "gwei"),
+			network_id: 1,
+		},
 		development: {
 			host: "127.0.0.1",
 			port: 7545,
 			network_id: "*",
+			gasPrice: web3.utils.toWei("120", "gwei"),
 		},
 		rinkeby: {
 			provider: function () {
-				return new HDWalletProvider(
-					process.env.MNEMONIC,
-					"https://rinkeby.infura.io/v3/18ddf63e78c54562abc1e640cb005c84",
-				);
+				return new HDWallet(process.env.MNEMONIC, process.env.PROVIDER_ADDR);
 			},
 			network_id: "4",
 			websockets: true,
